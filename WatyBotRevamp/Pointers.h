3022d6edd98f6b317ee506b4b7e70dc6985c2183
Wart Aarnoutse <wartaarnoutse@gmail.com>
2013-02-21 16:09:59 +0100

    Updated pointers to 88.2

--- WatyBotRevamp/Pointers.h
+++ WatyBotRevamp/Pointers.h
@@ -1,54 +1,52 @@
-#define CharBasePtr 0x011E1148
+#define CharBasePtr 0x011E2228
 #define AttackCountOffset 0x7034
 #define AttackXOffset 0x702C
 #define BuffCountOffset 0x7044
-#define TeleOffset 0xError
-#define TeleXOffset 0xError
-#define TeleYOffset 0xError
 #define BreathOffset 0x638
 #define ItemIDOffset 0x6D60
-#define XOffset 0x7B4C
-#define ComboOffset 0x7CC8
+#define XOffset 0x7B58
+#define ComboOffset 0x7CD0
 #define PetOffset 0x5050
 #define Pet1Offset 0x4
 #define PetFullness 0xB4
  
-#define InfoBasePtr 0x011EA2C8
-#define MapIDOffset 0xc90
-#define CharXOffset 0xB5C
+#define InfoBasePtr 0x011EB3A8
+#define MapIDOffset 0xC9C // new aob so could be wrong
+#define CharXOffset 0xB68
  
-#define StatsBasePtr 0x011E5AE8
-#define HPOffset 0x18C0
-#define MPOffset 0x18C4
-#define EXPOffset 0x17F0
+#define StatsBasePtr 0x011E6BC8
+#define HPOffset 0x18D8
+#define MPOffset 0x18DC
+#define EXPOffset 0x1808 //new aob so could be wrong
  
-#define MouseBasePtr 0x011E5F08
+#define MouseBasePtr 0x011E6FE8
 #define MouseLocOffset 0x978
 #define MouseXOffset 0x8C
 #define MouseAniOffset 0x9CC
  
-#define SettingsBasePtr 0x011E581C
+#define SettingsBasePtr 0x011E68FC
 #define HPAlertOffset 0x5C
 #define MPAlertOffset 0x60
  
-#define ServerBasePtr 0x011E0E90
+#define ServerBasePtr 0x011E1F70
 #define TubiOffset 0x2124
 #define WorldOffset 0x20A8
 #define ChannelOffset 0x20B0
  
-#define WallBasePtr 0x011E5744
+#define WallBasePtr 0x011E6824
 #define LeftWallOffset 0x1C
  
-#define MobBasePtr 0x011E5824
+#define MobBasePtr 0x011E6904
 #define MobCountOffset 0x10
  
-#define PeopleBasePtr 0x011E5820
+#define PeopleBasePtr 0x011E6900
 #define PeopleCountOffset 0x18
  
-#define ItemBasePtr 0x011E9F84
+#define ItemBasePtr 0x011EB064
 #define ItemCountOffset 0x14
  
-#define SendAddy 0x004CA9D0
-#define SendClassAddy 0x00490F27
-#define ClientSocketAddy 0x011E1238
+ 
+#define SendAddy 0x004C9C20
+#define SendClassAddy 0x00490777
+#define ClientSocketAddy 0x011E2318
  
