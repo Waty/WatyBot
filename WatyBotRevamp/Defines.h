4357c1e453ac8303f2c9f54037f217bac040e58d
Wart Aarnoutse <wartaarnoutse@gmail.com>
2013-03-12 17:40:23 +0100

    Added an AOB for CCing

--- WatyBotRevamp/Defines.h
+++ WatyBotRevamp/Defines.h
@@ -12,7 +12,7 @@ enum CCType{CC, CS, DC};
 #define ShowWarning(Message)	MessageBox::Show(Message, "Warning", MessageBoxButtons::OK, MessageBoxIcon::Warning)
 
 typedef void (__stdcall* PFN_CField_SendTransferChannelRequest)(unsigned char nChannel);
-PFN_CField_SendTransferChannelRequest CField_SendTransferChannelRequest = reinterpret_cast<PFN_CField_SendTransferChannelRequest>(0x00567AC0);
+PFN_CField_SendTransferChannelRequest CField_SendTransferChannelRequest = reinterpret_cast<PFN_CField_SendTransferChannelRequest>(0x00567AC0); //6A ?? 68 ?? ?? ?? ?? 64 A1 ?? ?? ?? ?? 50 83 EC ?? 56 57 A1 ?? ?? ?? ?? 33 C4 50 8D 44 24 ?? 64 A3 ?? ?? ?? ?? 8B 0D ?? ?? ?? ?? 85 C9
 
 unsigned long ReadPointer(unsigned long ulBase, int iOffset)
 {
