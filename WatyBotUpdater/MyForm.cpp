b9d2732f223e3e361b58d89f69daa2f0c33368f3
Wart Aarnoutse <wartaarnoutse@gmail.com>
2013-03-13 08:21:50 +0100

    Patternfinder works now

--- WatyBotUpdater/MyForm.cpp
+++ WatyBotUpdater/MyForm.cpp
@@ -1,41 +1,32 @@
 #include "MyForm.h"
 #include "PatternFind.h"
 using namespace WatyBotUpdater;
+#define ShowInfo(Message)		MessageBox::Show(Message, "Information", MessageBoxButtons::OK, MessageBoxIcon::Information)
+DWORD test;
 
-void Main(void)
-{
-	Application::EnableVisualStyles();
-	Application::SetCompatibleTextRenderingDefault(false);
-	Application::Run(gcnew MyForm);
-	Application::Exit();
-}
-
+PFSEARCH pf;
+void *lpvMapleBase = NULL;
+DWORD dwMapleSize = 0;
 void MyForm::bUpdate_Click(System::Object^  sender, System::EventArgs^  e)
 {
-	PFSEARCH pf; // This is the pattern search struct
-	LPVOID lpvMapleBase = NULL;
-	DWORD dwMapleSize = 0;
-
-	//DisableThreadLibraryCalls(hInstance);
-	GetModuleSize(GetModuleHandle(NULL), &lpvMapleBase, &dwMapleSize); // Obtain Maple base address & size
-	FindPattern("1a 2b 3c", &pf, lpvMapleBase, dwMapleSize);
+	lpvMapleBase = reinterpret_cast<LPVOID>(0x00400000);
+		
+	if(!FindPattern("6A ? 68 ? ? ? ? 64 A1 ? ? ? ? 50 83 EC ? 56 57 A1 ? ? ? ? 33 C4 50 8D 44 24 ? 64 A3 ? ? ? ? 8B 0D ? ? ? ? 85 C9", &pf, lpvMapleBase, dwMapleSize))
+	{
+		test = *(DWORD*)((DWORD)pf.lpvResult);
+		ShowInfo("CCAddy = " + test);
+	}
+	else ShowInfo("Error");
 }
 
 void InitializeTrainer(HINSTANCE hInstance)
 {
-	Main();
-    PFSEARCH pf; // This is the pattern search struct
-    LPVOID lpvMapleBase = NULL;
-    DWORD dwMapleSize = 0;
-
-	unsigned long ulMissGodmode;
-	unsigned long ulFullGodmode;
- 
     DisableThreadLibraryCalls(hInstance);
     GetModuleSize(GetModuleHandle(NULL), &lpvMapleBase, &dwMapleSize); // Obtain Maple base address & size
- 
-    FindPattern("8b 9c 24 ? 00 00 00 85 db 7f ? 8d", &pf, lpvMapleBase, dwMapleSize); // Perform search
-    ulMissGodmode = (unsigned long)pf.lpvResult; // The result is stored in the struct
-    FindPattern("0F 85 ? ? 00 00 8B 86 ? ? ? ? 83 E0 FE 83 F8 12 0F 84 ? ? 00 00 E8 ? ? ? ? 8B F8", &pf, lpvMapleBase, dwMapleSize);
-    ulFullGodmode = (unsigned long)pf.lpvResult;
+
+	//Run the form
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew MyForm);
+	Application::Exit();
 }
