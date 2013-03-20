18102656551892c359a7234b3a7365ba092a9931
Wart Aarnoutse <wartaarnoutse@gmail.com>
2013-03-20 08:37:43 +0100

    Updating to 89.2, not finished

--- WatyBotRevamp/Hacks.h
+++ WatyBotRevamp/Hacks.h
@@ -1,4 +1,5 @@
 #include "MobPool.h"
+#include "HackAddys.h"
 extern int getMapID();
 extern int getCharX();
 extern int getCharY();
@@ -6,11 +7,8 @@ extern int getAttackCount();
 
 namespace Hacks
 {
-	//Updated to V89.1
 	/////Dojang Godmode
-	DWORD DojangAddy = 0x00B60DF4;
 	DWORD DojangRet = DojangAddy + 5;
-	DWORD DojangCall = 0x00C228B0;
 	unsigned long DojangMissCount = 0;
 	CodeCave(Dojang)
 	{
@@ -27,12 +25,10 @@ namespace Hacks
 	CMemory cmDojangGodmode(DojangAddy, CaveDojang, 0, true);
  
 	/////Auto Aggro
-	DWORD dwAggroAddy = 0x00C3755F;
 	DWORD dwAggroRet = dwAggroAddy + 5;
-	DWORD dwAggroCall = 0x00C2E020;
 	CodeCave(Aggro)
 	{
-		call dwAggroCall
+		call AggroCall
 		mov edx,dword ptr ds:[MobBasePtr]
 		mov edx,[edx+0x29D8]
 		mov edx,[edx+0x0C]
@@ -40,16 +36,13 @@ namespace Hacks
 		jmp dwAggroRet
 	}
 	EndCodeCave
-	CMemory cmAutoAggro(dwAggroAddy, CaveAggro, 0, true);
+	CMemory cmAutoAggro(AggroAddy, CaveAggro, 0, true);
  
 	/////Pin Typer
-	DWORD dwPinTyper1 = 0x00690426;
-	DWORD dwPinTyper2 = 0x00691AF7;
 	BYTE bPinTyper[] = {0x0F, 0x84};
-	CMemory cmPinTyper(dwPinTyper1, bPinTyper, 2, dwPinTyper2, bPinTyper, 2);
+	CMemory cmPinTyper(PinTyperAddy1, bPinTyper, 2, PinTyperAddy2, bPinTyper, 2);
  
 	/////FusionAttack
-	DWORD dwFusionAddy = 0x00709002;
 	DWORD dwFusionRet = dwFusionAddy + 8;
 	CodeCave(FusionAttack)
 	{
@@ -62,94 +55,72 @@ namespace Hacks
 		jmp dwFusionRet
 	}
 	EndCodeCave
-	CMemory cmFusionAttack(dwFusionAddy, CaveFusionAttack, 3, true);
+	CMemory cmFusionAttack(FusionAddy, CaveFusionAttack, 3, true);
  
 	/////Perfect Loot
-	DWORD dwPerfectLoot1 = 0x004B5367;// 89 86 ?? ?? ?? ?? E8 ?? ?? ?? ?? 89 ?? ?? ?? ?? ?? 5E C2
-	DWORD dwPerfectLoot2 = 0x00542805;// 0D ?? ?? ?? ?? 83 C4 ?? E9 ?? ?? ?? ?? DD 05 ?? ?? ?? ?? DC C9
-	DWORD dwPerfectLoot3 = 0x004440D9;// 81 FE ?? ?? ?? ?? 0F 8D ?? ?? ?? ?? 85 ED 0F 84 ?? ?? ?? ?? 81 FE ?? ?? ?? ?? BF ?? ?? ?? ?? 7E ?? B8 ?? ?? ?? ?? 2B C6
 	BYTE bPerfectLoot1[] = {0x90, 0x90, 0x90, 0x90, 0x90, 0x90};
 	BYTE bPerfectLoot2[] = {0x25};
 	BYTE bPerfectLoot3[] = {0x81, 0xFE, 0x00, 0x00, 0x00, 0x00};
-	CMemory cmPerfectLoot(dwPerfectLoot1, bPerfectLoot1, sizeof(bPerfectLoot1), dwPerfectLoot2, bPerfectLoot2, sizeof(bPerfectLoot2), dwPerfectLoot3, bPerfectLoot3, sizeof(bPerfectLoot3));
+	CMemory cmPerfectLoot(PerfectLootAddy1, bPerfectLoot1, sizeof(bPerfectLoot1), PerfectLootAddy2, bPerfectLoot2, sizeof(bPerfectLoot2), PerfectLootAddy3, bPerfectLoot3, sizeof(bPerfectLoot3));
  
 	/////No Background + Clouds
-	DWORD dwNoBG1 = 0x006B826C;
-	DWORD dwNoBG2 = 0x006B6B7B;
 	BYTE bNoBG[] = {0x90, 0x90, 0x90, 0x90, 0x90};
-	CMemory cmNoBG(dwNoBG1, bNoBG, 5, dwNoBG2, bNoBG, 5);
+	CMemory cmNoBG(NoBGAddy1, bNoBG, 5, NoBGAddy2, bNoBG, 5);
  
 	/////Faster Mobs
-	DWORD dwFasterMobs = 0x0071E5D3;
 	BYTE bFasterMobs[] = {0x90, 0x90};
-	CMemory cmFasterMobs(dwFasterMobs, bFasterMobs, 2);
+	CMemory cmFasterMobs(FasterMobsAddy, bFasterMobs, 2);
  
 	/////Unlimited Morp
-	DWORD dwMorph1 = 0x00BC91A8;
-	DWORD dwMorph2 = 0x00BC9926;
 	BYTE bMorph1[] = {0xEB, 0x2E};
 	BYTE bMorph2[] = {0xEB};
-	CMemory cmUnlimitedMorph(dwMorph1, bMorph1, 2, dwMorph2, bMorph1, 1);
+	CMemory cmUnlimitedMorph(UnlimitedMorphAddy1, bMorph1, 2, UnlimitedMorphAddy2, bMorph2, 1);
  
 	/////ND - All Attacks
-	DWORD dwNDAllAttacks = 0x00BA2926;
 	BYTE bNDAllAttacks[] = {0x74};
-	CMemory cmNDAllAttacks(dwNDAllAttacks, bNDAllAttacks, 1);
+	CMemory cmNDAllAttacks(NDAllAttacksAddy, bNDAllAttacks, 1);
  
 	/////Jump Down Anywhere
-	DWORD dwJDA1 = 0x00B6F6E9;
-	DWORD dwJDA2 = 0x00B6F726;
-	DWORD dwJDA3 = 0x00B76ED4;
 	BYTE bJDA1[] = {0xEB};
 	BYTE bJDA2[] = {0xEB};
 	BYTE bJDA3[] = {0x90, 0x90};
-	CMemory cmJDA(dwJDA1, bJDA1, 1, dwJDA2, bJDA2, 1, dwJDA3, bJDA3, 2);
+	CMemory cmJDA(JDAAddy1, bJDA1, 1, JDAAddy2, bJDA2, 1, JDAAddy3, bJDA3, 2);
  
 	/////Full Mob Disarm
-	DWORD dwDisarm = 0x00706D02;
 	BYTE bDisarm[] = {0xE9, 0x9B, 0x02, 0x00, 0x00, 0x90, 0x90, 0x90, 0x90};
-	CMemory cmMobDisarm(dwDisarm, bDisarm, sizeof(bDisarm));
+	CMemory cmMobDisarm(MobDisarmAddy, bDisarm, sizeof(bDisarm));
  
-	/////No Mobs
-	DWORD dwNoMobs = 0x0070B2FD;// 77 ? 0F B6 80 ? ? ? 00 FF 24 85 ? ? ? 00 8B 54 24 ? 52 E8 ? ? ? FF C2 08 00 - 2nd result
+	/////No Mobs			77 ? 0F B6 80 ? ? ? 00 FF 24 85 ? ? ? 00 8B 54 24 ? 52 E8 ? ? ? FF C2 08 00 - 2nd result
 	BYTE bNoMobs[] = {0xEB};
 	CMemory cmNoMobs(dwNoMobs, bNoMobs, 1);
  
 	/////Instant Air Loot
-	DWORD dwAirLoot = 0x00543999;// 75 68 2B 56 ** 81 FA ** ** ** ** 7C ** 8B ** ** 3D
 	BYTE bAirLoot[] = {0x74};
-	CMemory cmInstantAirLoot(dwAirLoot, bAirLoot, 1);
+	CMemory cmInstantAirLoot(AirLootAddy, bAirLoot, 1);
  
 	/////Vac Right
-	DWORD dwVacRight = 0x00C226A0;
 	BYTE bVacRight[] = {0x75, 0x48};
-	CMemory cmVacRight(dwVacRight, bVacRight, 2);// 75 ?? DC 11 DF E0 F6 C4 41 75 ?? D9 C9 DC 74 24 10 DC 4C 24 20 DC 01 DD 11 D8 D9 DF E0 F6 C4 41
+	CMemory cmVacRight(VacRightAddy, bVacRight, 2);// 75 ?? DC 11 DF E0 F6 C4 41 75 ?? D9 C9 DC 74 24 10 DC 4C 24 20 DC 01 DD 11 D8 D9 DF E0 F6 C4 41
  
 	/////Walk Unrandom Right
-	DWORD dwWalkRight = 0x00C2C98B;
 	BYTE bWalkRight[] = {0xE9, 0x95, 0x00, 0x00, 0x00, 0x90};
-	CMemory cmWalkRight(dwWalkRight, bWalkRight, 6);
+	CMemory cmWalkRight(WalkRightAddy, bWalkRight, 6);
  
 	/////Jump Unrandom Right
-	DWORD dwJumpRight = 0x00C2CE8F;
 	BYTE bJumpRight[] = {0xE9, 0x7A, 0x02, 0x00, 0x00, 0x90};
-	CMemory cmJumpRight(dwJumpRight, bJumpRight, 6);
+	CMemory cmJumpRight(JumpRightAddy, bJumpRight, 6);
  
 	/////NoKB
-	DWORD dwNoKB = 0x00887DFB;
 	BYTE bNoKB[] = {0x00};
-	CMemory cmNoKB(dwNoKB, bNoKB, 1);
+	CMemory cmNoKB(NoKBAddy, bNoKB, 1);
  
-	/////Sit Everywhere
-	DWORD dwSit = 0x00B65EE1;
+	/////Sit Hack
 	BYTE bSit[] = {0x75};
-	CMemory cmSitHack(dwSit, bSit, 1);
+	CMemory cmSitHack(SitHackAddy, bSit, 1);
  
 	/////Spawn Control
-	DWORD dwSPControl = 0x00B8A17A;
 	DWORD dwSPControlRet = dwSPControl + 6;
 	int spawn_x, spawn_y;
- 
 	BOOL WINAPI GetCoords()
 	{
 		int iMapID = getMapID();
@@ -183,14 +154,12 @@ namespace Hacks
 		jmp [dwSPControlRet]
 	}
 	EndCodeCave
-	CMemory cmSPControl(dwSPControl, CaveSPControl, 1, true);
+	CMemory cmSPControl(SPControlAddy, CaveSPControl, 1, true);
  
 	/////50 Seconds Godmode
-	DWORD dw50SecGM1 = 0x00BA138E;
-	DWORD dw50SecGM2 = 0x00BA13A9;
 	BYTE b50SecGM1[] = {0x7E};
 	BYTE b50SecGM2[] = {0xD4, 0x36};
-	CMemory cm50SecGM(dw50SecGM1, b50SecGM1, 1, dw50SecGM2, b50SecGM2, 2);
+	CMemory cm50SecGM(Godmode50SecAddy1, b50SecGM1, 1, Godmode50SecAddy2, b50SecGM2, 2);
  
 	/////Logo Skipper
 	DWORD dwLogoSkipper = 0x0069FAA0;
