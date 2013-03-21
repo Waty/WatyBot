1c378231062df48fdb727bd9d2bac068b3325a72
Wart Aarnoutse <wartaarnoutse@gmail.com>
2013-03-21 18:46:09 +0100

    Releaseo of WatyBot 2.6 EMS 89.2

--- WatyBotRevamp/MainForm.h
+++ WatyBotRevamp/MainForm.h
@@ -2479,7 +2479,7 @@ private: System::Windows::Forms::CheckBox^  cbKami;
 			this->Name = L"MainForm";
 			this->ShowIcon = false;
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
-			this->Text = L"WatyBot - V2.5.1 - CCPLZ! - EMS 89.1";
+			this->Text = L"WatyBot - V2.6 - CCPLZ! - EMS 89.1";
 			this->FormClosing += gcnew System::Windows::Forms::FormClosingEventHandler(this, &MainForm::MainForm_FormClosing);
 			this->Load += gcnew System::EventHandler(this, &MainForm::MainForm_Load);
 			this->MainTabControl->ResumeLayout(false);
