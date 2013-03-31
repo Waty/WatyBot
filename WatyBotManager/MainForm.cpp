0c9ddf5f24849a98eadae42001c9f33319e78f7e
Wart Aarnoutse <wartaarnoutse@gmail.com>
2013-03-31 21:40:05 +0200

    the concept of WatyBotManager, still needs lots of tweaking

--- WatyBotManager/MainForm.cpp
+++ WatyBotManager/MainForm.cpp
@@ -1,21 +1,95 @@
 #include "MainForm.h"
+#include <string>
+#include <vector>
+using namespace std;
+using namespace System::Diagnostics;
 using namespace WatyBotManager;
 
+#define file "E:\\Games\\Europe MapleStory\\MapleStory.exe"
+DWORD dwProcessId;
+
+BOOL IsElevated( ) {
+	BOOL fRet = FALSE;
+	HANDLE hToken = NULL;
+	if( OpenProcessToken( GetCurrentProcess( ),TOKEN_QUERY,&hToken ) ) {
+		TOKEN_ELEVATION Elevation;
+		DWORD cbSize = sizeof( TOKEN_ELEVATION );
+		if( GetTokenInformation( hToken, TokenElevation, &Elevation, sizeof( Elevation ), &cbSize ) ) {
+			fRet = Elevation.TokenIsElevated;
+		}
+	}
+	if( hToken ) {
+		CloseHandle( hToken );
+	}
+	return fRet;
+}
+
+static BOOL bChild = FALSE;
+static HWND hMapleStory, hParent;
+void EmbedMapleStory(HWND hWnd)
+{
+    if(!bChild)
+    {
+        if(hMapleStory)
+        {
+            hParent = SetParent(hMapleStory, hWnd);
+            if(hParent)
+            {
+                bChild = TRUE;
+                SetWindowPos(hMapleStory, 0, 3, -17, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+            }
+        }
+    }
+    else
+    {
+        SetParent(hMapleStory, hParent);
+        bChild = FALSE;
+    }
+}
+
+HWND FindProcessWindow(int pID)
+{
+   TCHAR szBuffer[200];
+   DWORD dwTemp;
+
+   for (HWND hWnd = GetTopWindow(NULL); hWnd != NULL; hWnd = GetNextWindow(hWnd, GW_HWNDNEXT))
+   {
+      GetWindowThreadProcessId(hWnd, &dwTemp);
+
+      if (dwTemp != pID)
+		  continue;
+
+      if (!GetClassName(hWnd, szBuffer, sizeof(szBuffer) / sizeof(TCHAR)))
+		  continue;
+
+      if (!wcscmp(szBuffer, L"MapleStoryClass"))
+		  return hWnd;
+   }
+   return NULL;
+}
+
+[STAThread]
 void Main()
 {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
-	Application::Run(gcnew MainForm);
+	if(IsElevated()) Application::Run(gcnew MainForm);
+	else MessageBox::Show("Run as administrator!");
 	Application::Exit();
 }
 
-void MainForm::MainForm_Resize(System::Object^  sender, System::EventArgs^  e)
+void MainForm::bStartMS_Click(System::Object^  sender, System::EventArgs^  e)
 {
-	this->tabControl1->Width = this->Width - 16;
-	this->tabControl1->Height = this->Height - 40;
-}
+	//Start maplestory
+	Process^ maplestory = gcnew Process();
+	maplestory->StartInfo->FileName = file;
+	maplestory->Start();
+	WaitForInputIdle((HANDLE) maplestory->Handle.ToPointer(), INFINITE);
 
-void MainForm::startNewTabToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e)
-{
-	::MessageBox::Show("No");
+	//Store the HWND of the launcher + closing it
+	maplestory->CloseMainWindow();
+	
+	while(FindProcessWindow(maplestory->Id) == NULL);
+	hMapleStory = FindProcessWindow(maplestory->Id);
+	EmbedMapleStory((HWND) panel1->Handle.ToPointer());	
 }
--- WatyBotManager/MainForm.h
+++ WatyBotManager/MainForm.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <Windows.h>
 
 namespace WatyBotManager {
 
@@ -35,12 +36,10 @@ namespace WatyBotManager {
 			}
 		}
 	private: System::Windows::Forms::TabControl^  tabControl1;
-	protected: 
 	private: System::Windows::Forms::TabPage^  tabPage1;
 	private: System::Windows::Forms::TabPage^  tabPage2;
-	private: System::Windows::Forms::MenuStrip^  menuStrip1;
-	private: System::Windows::Forms::ToolStripMenuItem^  startNewTabToolStripMenuItem;
-
+	private: System::Windows::Forms::Button^  bStartMS;
+	private: System::Windows::Forms::Panel^  panel1;
 	private:
 		/// <summary>
 		/// Required designer variable.
@@ -56,80 +55,235 @@ namespace WatyBotManager {
 		{
 			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
 			this->tabPage1 = (gcnew System::Windows::Forms::TabPage());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
 			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
-			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
-			this->startNewTabToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->bStartMS = (gcnew System::Windows::Forms::Button());
 			this->tabControl1->SuspendLayout();
-			this->menuStrip1->SuspendLayout();
+			this->tabPage1->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// tabControl1
 			// 
 			this->tabControl1->Controls->Add(this->tabPage1);
 			this->tabControl1->Controls->Add(this->tabPage2);
-			this->tabControl1->Location = System::Drawing::Point(1, 22);
+			this->tabControl1->Location = System::Drawing::Point(2, 2);
 			this->tabControl1->Name = L"tabControl1";
 			this->tabControl1->SelectedIndex = 0;
-			this->tabControl1->Size = System::Drawing::Size(402, 206);
+			this->tabControl1->Size = System::Drawing::Size(1088, 670);
 			this->tabControl1->TabIndex = 0;
 			// 
 			// tabPage1
 			// 
+			this->tabPage1->Controls->Add(this->panel1);
 			this->tabPage1->Location = System::Drawing::Point(4, 22);
 			this->tabPage1->Name = L"tabPage1";
 			this->tabPage1->Padding = System::Windows::Forms::Padding(3);
-			this->tabPage1->Size = System::Drawing::Size(394, 180);
+			this->tabPage1->Size = System::Drawing::Size(1080, 644);
 			this->tabPage1->TabIndex = 0;
 			this->tabPage1->Text = L"tabPage1";
 			this->tabPage1->UseVisualStyleBackColor = true;
+			// 
+			// panel1
+			// 
+			this->panel1->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->panel1->Location = System::Drawing::Point(6, 8);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(1068, 631);
+			this->panel1->TabIndex = 2;
 			// 
 			// tabPage2
 			// 
 			this->tabPage2->Location = System::Drawing::Point(4, 22);
 			this->tabPage2->Name = L"tabPage2";
 			this->tabPage2->Padding = System::Windows::Forms::Padding(3);
-			this->tabPage2->Size = System::Drawing::Size(394, 175);
+			this->tabPage2->Size = System::Drawing::Size(838, 623);
 			this->tabPage2->TabIndex = 1;
 			this->tabPage2->Text = L"tabPage2";
 			this->tabPage2->UseVisualStyleBackColor = true;
 			// 
-			// menuStrip1
+			// bStartMS
 			// 
-			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) {this->startNewTabToolStripMenuItem});
-			this->menuStrip1->Location = System::Drawing::Point(0, 0);
-			this->menuStrip1->Name = L"menuStrip1";
-			this->menuStrip1->RenderMode = System::Windows::Forms::ToolStripRenderMode::System;
-			this->menuStrip1->Size = System::Drawing::Size(402, 24);
-			this->menuStrip1->TabIndex = 1;
-			this->menuStrip1->Text = L"menuStrip1";
-			// 
-			// startNewTabToolStripMenuItem
-			// 
-			this->startNewTabToolStripMenuItem->Name = L"startNewTabToolStripMenuItem";
-			this->startNewTabToolStripMenuItem->Size = System::Drawing::Size(88, 20);
-			this->startNewTabToolStripMenuItem->Text = L"Start new tab";
-			this->startNewTabToolStripMenuItem->Click += gcnew System::EventHandler(this, &MainForm::startNewTabToolStripMenuItem_Click);
+			this->bStartMS->Location = System::Drawing::Point(867, -1);
+			this->bStartMS->Name = L"bStartMS";
+			this->bStartMS->Size = System::Drawing::Size(224, 23);
+			this->bStartMS->TabIndex = 1;
+			this->bStartMS->Text = L"Start MapleStory";
+			this->bStartMS->UseVisualStyleBackColor = true;
+			this->bStartMS->Click += gcnew System::EventHandler(this, &MainForm::bStartMS_Click);
 			// 
 			// MainForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(402, 228);
+			this->ClientSize = System::Drawing::Size(1090, 674);
+			this->Controls->Add(this->bStartMS);
 			this->Controls->Add(this->tabControl1);
-			this->Controls->Add(this->menuStrip1);
-			this->MainMenuStrip = this->menuStrip1;
 			this->Name = L"MainForm";
-			this->Text = L"MainForm";
-			this->Resize += gcnew System::EventHandler(this, &MainForm::MainForm_Resize);
+			this->Text = L"$W4GB0t2013YOLO";
 			this->tabControl1->ResumeLayout(false);
-			this->menuStrip1->ResumeLayout(false);
-			this->menuStrip1->PerformLayout();
+			this->tabPage1->ResumeLayout(false);
 			this->ResumeLayout(false);
-			this->PerformLayout();
 
 		}
 #pragma endregion
-	private: System::Void MainForm_Resize(System::Object^  sender, System::EventArgs^  e);
-	private: System::Void startNewTabToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e); 
+private: System::Void bStartMS_Click(System::Object^  sender, System::EventArgs^  e);
+private: System::Void cbCPUHack_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void bEmbedMS_Click(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void bSafetySwitch_Click(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbTeleDelay_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void nudTeleDelay_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbUnlAtk_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbSiegeMode_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbJDA_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void bItemVacChar_Click(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbFLACC_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void nudGMMisses_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void nudItemVacY_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbDojangGM_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbDelayGM_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void nudGMSecs_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void nudItemVacX_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbKami_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbItemVac_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbSwimHack_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void nudKamiXRange_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbKamiLoot_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void nudKamiYRange_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbPerfectLoot_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void nudKamiItems_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbNoCharKB_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbSuspendMobs_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void bKBFHCharFH_Click(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void bMMCCharPos_Click(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbKBFHVac_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbMobFreeze_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbFilterMobs_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void nudKBFH_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbFasterMobs_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbLemmings_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbNoMobKB_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbVacRight_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbMobDisarm_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void nudMMCY_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void nudMMCX_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbMMC_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbWUVac_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbJUVac_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void nudSlwib_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void nudSawsil_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbAutoCCDelay_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbAutoCCPeople_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void nudCCDelay_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void nudCCPeople_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbMPKey_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void nudMP_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbHPKey_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void nudHP_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbAutoPot_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbLootKey_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void nudLootDelay_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbAutoLoot_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbAttackKey_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void nudAttackDelay_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbAutoAttack_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbAutoSkill2_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbAutoSkill1_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbAutoSkill4_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbAutoSkill3_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbAutoSkill_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbSkillKey2_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbSkillKey3_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbSkillKey4_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbSkillKey1_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void nudSkillDelay1_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void nudSkillDelay4_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void nudSkillDelay3_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void nudSkillDelay2_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbSkillKamiDisable_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void bClearAll_Click(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void nudMesoFilter_ValueChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbMesoFilter_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void cbItemFilter_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void bRemoveItem_Click(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void bAddItem_Click(System::Object^  sender, System::EventArgs^  e) {
+		 }
 };
 }
