a206ffb82c3ae1e75cde76f3b37a83598e07abaa
Wart Aarnoutse <wartaarnoutse@gmail.com>
2013-04-07 14:24:27 +0200

    Enabled /CLR, so I can use .Net outside the MainForm

--- WatyBotRevamp/MainDll.cpp
+++ WatyBotRevamp/MainDll.cpp
@@ -1,12 +1,7 @@
 #include <Windows.h>
+#pragma unmanaged
 
 extern void Main(void);
-
-::BOOL WINAPI DllWork ( __in ::HMODULE hModule )
-{
-	Main();
-	return true;
-}
 
 ::BOOL WINAPI DllMain ( __in ::HMODULE hModule, __in ::DWORD dwReason, __in __reserved ::LPVOID lpvReserved )
 {
@@ -14,7 +9,7 @@ extern void Main(void);
 
 	if ( dwReason == DLL_PROCESS_ATTACH ) 
 	{
-		if (( hThread = ::CreateThread(NULL, 0, (::LPTHREAD_START_ROUTINE)&DllWork, (::HMODULE)hModule, 0, NULL) ) == NULL ) 
+		if (( hThread = ::CreateThread(NULL, 0, (::LPTHREAD_START_ROUTINE)&Main, (::HMODULE)hModule, 0, NULL) ) == NULL ) 
 		{
 			return FALSE;
 		}
