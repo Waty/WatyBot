2ea82527f14c7ae765c0fd60a22740e299d90eb2
Wart Aarnoutse <wartaarnoutse@gmail.com>
2013-04-24 22:23:59 +0200

    Added a post-build bat file

---
+++ Build.bat
@@ -0,0 +1,4 @@
+@echo off
+xcopy /y "E:\Games\Europe MapleStory\WatyBot.dll" "E:\Games\Europe MapleStory\Dlls\WatyBot.dll"
+xcopy /y "E:\Games\Europe MapleStory\WatyBotInjector.exe" "E:\Games\Europe MapleStory\Dlls\WatyBotInjector.exe"
+xcopy /y "E:\Games\Europe MapleStory\WatyBotUpdater.dll" "E:\Games\Europe MapleStory\Dlls\WatyBotUpdater.dll"
