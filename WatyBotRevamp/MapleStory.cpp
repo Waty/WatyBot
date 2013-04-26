25e7152dc873017245c11bb215340c2a5ed75cdb
Wart Aarnoutse <wartaarnoutse@gmail.com>
2013-04-26 13:19:20 +0200

    Added some more spacing

--- WatyBotRevamp/MapleStory.cpp
+++ WatyBotRevamp/MapleStory.cpp
@@ -6,10 +6,10 @@ CMapleStory::CMapleStory(void)
 {
 }
 
-
 CMapleStory::~CMapleStory(void)
 {
 }
+
 unsigned long CMapleStory::ReadPointer(unsigned long ulBase, int iOffset)
 {
 	if(*(int*)WallBasePtr)
@@ -19,11 +19,13 @@ unsigned long CMapleStory::ReadPointer(unsigned long ulBase, int iOffset)
 	}
 	else return 0;
 }
+
 double CMapleStory::ReadDoublePointer(DWORD ulBase, INT iOffset)
 {
       __try { return (*(DOUBLE*)(*(DWORD*)ulBase + iOffset)); }
       __except (EXCEPTION_EXECUTE_HANDLER) { return 0.0; }
 }
+
 bool CMapleStory::WritePointer(unsigned long ulBase, int iOffset, int iValue)
 {
 	__try
@@ -36,6 +38,7 @@ bool CMapleStory::WritePointer(unsigned long ulBase, int iOffset, int iValue)
 		return false;
 	}
 }
+
 bool CMapleStory::FindProcessWindow()
 {
    TCHAR szBuffer[200];
@@ -65,81 +68,97 @@ bool CMapleStory::FindProcessWindow()
    return false;
 }
 
-
 int CMapleStory::getMobCount()
 {
 	return (int) ReadPointer(MobBasePtr, MobCountOffset);
 }
+
 int CMapleStory::getItemCount()
 {
 	return (int) ReadPointer(ItemBasePtr, ItemCountOffset);
 }
+
 int CMapleStory::getPeopleCount()
 {
 	return (int) ReadPointer(PeopleBasePtr, PeopleCountOffset);
 }
+
 int CMapleStory::getCharX()
 {
 	return (int) ReadPointer(CharBasePtr,XOffset);
 }
+
 int CMapleStory::getCharY()
 {
 	return (int) ReadPointer(CharBasePtr,XOffset + 4);
 }
+
 int CMapleStory::getCharHP()
 {
 	WritePointer(SettingsBasePtr, HPAlertOffset, 20);
 	return (int) ReadPointer(StatsBasePtr, HPOffset);
 }
+
 int CMapleStory::getCharMP()
 {
 	WritePointer(SettingsBasePtr, MPAlertOffset, 20);
 	return (int) ReadPointer(StatsBasePtr, MPOffset);
 }
+
 double CMapleStory::getCharEXP()
 {
 	return ReadDoublePointer(StatsBasePtr, EXPOffset);
 }
+
 int CMapleStory::getMapID()
 {
 	return (int) ReadPointer(InfoBasePtr, MapIDOffset);
 }
+
 int CMapleStory::getAttackCount()
 {
 	return (int) ReadPointer(CharBasePtr, AttackCountOffset);
 }
+
 int CMapleStory::getTubiValue()
 {
 	return (int) ReadPointer(ServerBasePtr, TubiOffset);
 }
+
 int CMapleStory::getBreathValue()
 {
 	return (int) ReadPointer(CharBasePtr, BreathOffset);
 }
+
 int CMapleStory::getChannel()
 {
 	return (int) ReadPointer(ServerBasePtr, ChannelOffset);
 }
+
 int CMapleStory::getCharpID()
 {
 	if(*(int*)WallBasePtr)	return (int) ReadPointer(CharBasePtr, pIDOffset);
 	else return 0;
 }
+
 int CMapleStory::getKnockBack()
 {
 	if(*(int*)WallBasePtr)	return (int) ReadPointer(getCharpID(), KBOffset);
 	else return 0;
 }
+
 int CMapleStory::getKnockBackX()
 {
 	if(*(int*)WallBasePtr)	return (int) ReadPointer(getCharpID(), KBXOffset);
 	else return 0;
 }
+
 int CMapleStory::getKnockBackY()
 {
 	if(*(int*)WallBasePtr)	return (int) ReadPointer(getCharpID(), KBYOffset);
 	else return 0;
 }	
+
 bool CMapleStory::isInGame()
 {
 	if(getMapID() > 0)	return true;
