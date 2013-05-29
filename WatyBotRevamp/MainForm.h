f71a49d2fa59d034a2b72d259e61aceca214b607
Wart Aarnoutse <wartaarnoutse@gmail.com>
2013-05-29 21:51:04 +0200

    Release of 3.1.1

--- WatyBotRevamp/MainForm.h
+++ WatyBotRevamp/MainForm.h
@@ -2331,7 +2331,7 @@ private:
 			this->MaximizeBox = false;
 			this->Name = L"MainForm";
 			this->ShowIcon = false;
-			this->Text = L"WatyBot 3.1 - CCPLZ! - EMS 90.1";
+			this->Text = L"WatyBot 3.1.1 - CCPLZ! - EMS 90.1";
 			this->FormClosing += gcnew System::Windows::Forms::FormClosingEventHandler(this, &MainForm::MainForm_FormClosing);
 			this->Load += gcnew System::EventHandler(this, &MainForm::MainForm_Load);
 			this->MainTabControl->ResumeLayout(false);
