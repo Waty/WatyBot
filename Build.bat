53d3908a7d98943a9b0d2cd6b2c1725bd764baa0
Wart Aarnoutse <wartaarnoutse@gmail.com>
2013-09-06 19:26:41 +0200

    Cleaned up

--- Build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d d:
-cd "D:\Programs\Europe MapleStory\"
-copy WatyBot.dll Dlls\ /Y
-copy WatyBotInjector.exe Dlls\ /Y
-copy WatyBotManager.exe Dlls\ /Y
-copy WatyBotUpdater.dll Dlls\ /Y
