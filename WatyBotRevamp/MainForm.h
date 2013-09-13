71a7a91b15fec1c1742ccccf5908468bcf5af5ee
Wart Aarnoutse <wartaarnoutse@gmail.com>
2013-09-13 16:52:53 +0200

    Released WatyBot 3.4.1
    HotFix for IFS

--- WatyBotRevamp/MainForm.h
+++ WatyBotRevamp/MainForm.h
@@ -2147,7 +2147,7 @@ namespace WatyBotRevamp {
 			this->MaximizeBox = false;
 			this->Name = L"MainForm";
 			this->ShowIcon = false;
-			this->Text = L"WatyBot 3.4 - CCPLZ! - EMS 94.1";
+			this->Text = L"WatyBot 3.4.1 - CCPLZ! - EMS 94.1";
 			this->FormClosing += gcnew System::Windows::Forms::FormClosingEventHandler(this, &MainForm::MainForm_FormClosing);
 			this->Load += gcnew System::EventHandler(this, &MainForm::MainForm_Load);
 			this->MainTabControl->ResumeLayout(false);
